a70578056ba4297734c0f45eba5a27efb688fcdf
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-04-16 16:53:23 +0400

    added getter functions

--- model/io.js
+++ model/io.js
@@ -16,11 +16,47 @@ function get_org_feature(firebasepath, org, feature){
 	});
 }
 
+// Given an organization, returns a JSON object, with key = featurename, and value = timeseries.
 function get_org(firebasepath, org) {
+	// Instantiate orgpath
+	var orgpath = firebasepath + "/orgs/" + org + /data/;
+	var orgRef = new Firebase(orgpath);
+
+	orgRef.orderByKey().on("value", function (snapshot) {
+		p = snapshot.val()
+		var results = new Object()
+		// For each feature, grab the feature name and the series
+		for (var feature in p) {
+		  if (p.hasOwnProperty(feature)) {
+		  	results[feature] = p[feature]["series"]
+		  }
+		}
+		//console.log(results)
+		return results
+	})
 	return
 }
 
-
+// Returns a JSON Object where the key is the org, and the val is the series.
 function get_feature(firebasepath, feature){
-	return
+
+	// Instantiate orgpath
+	var orgpath = firebasepath + "/orgs/";
+	var orgRef = new Firebase(orgpath);
+
+	// Query orgs that have the feature
+	orgRef.orderByChild("data/" + feature + "/name").equalTo(feature).on("value", function (snapshot) {
+		//console.log(snapshot.val())
+		p = snapshot.val()
+		var results = new Object()
+		// For each org, grab the orgname and the series
+		for (var orgname in p) {
+		  if (p.hasOwnProperty(orgname)) {
+		  	results[orgname] = p[orgname]["data"][feature]["series"]
+		    //console.log(orgname + " -> " + p[orgname]);
+		  }
+		}
+		//console.log(results)
+		return results
+	});
 }
